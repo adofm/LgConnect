292e790a2c7aa43476b4ac0e5775229559f71b0a
adofm <karthik.maniam008@gmail.com>
2025-06-08 13:38:54 +0530

    feat: implement AuthImagePattern component for authentication pages

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,15 @@
+import main from "../components/lg.png";
+
+const AuthImagePattern = ({ title, subtitle }) => {
+  return (
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+      <div className="max-w-md text-center">
+       <img src={main}/>
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthImagePattern;
